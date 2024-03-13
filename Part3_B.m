5e10e91899360347834c88d3bec014c614d2e263
Vikram Kannur <148599421+Runway27@users.noreply.github.com>
2024-03-13 10:13:37 +0000

    Update Part3_B.m

--- Part3_B.m
+++ Part3_B.m
@@ -13,19 +13,36 @@ Date=Date07;
 [Xnorm, norm_params] = mapstd(Xsel'); 
 Xnorm=Xnorm'; 
 
+% Create a lagged version of the dataset
+Xlagged = [NaN(1, size(Xnorm, 2)); Xnorm(1:end-1, :)];
+
+% Combine the current and lagged datasets
+XnormLagged = [Xnorm, Xlagged];
+
+% Remove the first row, which contains NaN due to the lag
+XnormLagged(1, :) = [];
+
+% Similarly, remove the first row from the output variable
+Ylagged = Y(2:end);
+
 % Split the data into training, validation, and test sets
-s2014=find(year(Date)==2014,1,'first');
+s2014=find(year(Date)==2014,2,'first');
 f2015=find(year(Date)==2015,1,'last');
 s2016=find(year(Date)==2016,1,'first');
 f2016=find(year(Date)==2016,1,'last');
 s2017=find(year(Date)==2017,1,'first');
 f2018=find(year(Date)==2018,1,'last');
-XTrain=Xnorm(s2014:f2015,:);
-XVal=Xnorm(s2016:f2016,:);
-XTest=Xnorm(s2017:f2018,:);
-YTrain=Y(s2014:f2015);
-YVal=Y(s2016:f2016);
-YTest=Y(s2017:f2018);
+XTrain=XnormLagged(s2014:f2015,:);
+XVal=XnormLagged(s2016:f2016,:);
+% Adjust the indices for the test set
+s2017=find(year(Date)==2017,1,'first') - 1;
+f2018=find(year(Date)==2018,1,'last') - 1;
+
+% Now, use these adjusted indices to create the test set
+XTest=XnormLagged(s2017:f2018,:);
+YTrain=Ylagged(s2017:f2018);
+YVal=Ylagged(s2016:f2016);
+YTest=Ylagged(s2017:f2018);
 
 % Define the range for the number of hidden neurons
 hiddenLayerSizeRange = 1:20;
